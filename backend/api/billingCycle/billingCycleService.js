fb4c3b382d3a5be985ca8e2636ab66468753e517
Samael Pereira Simoes <samael.simoes@gmail.com>
2020-02-24 16:21:16 -0300

    Ajusted back end and creat front end

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -3,6 +3,24 @@ const BillingCycle = require('./billingCycle');
 //Node criar uma api rest usando o padrão do node
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
 BillingCycle.updateOptions({new: true, runValidators: true});
+BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext);
+
+function sendErrorsOrNext(req, res, next) {
+    const bundle = res.locals.bundle;
+
+    if (bundle.errors) {
+        var erros = parseErros(bundle.errors);
+        res.status(500).json({errors});
+    } else {
+        next();
+    }
+}
+
+function parseErros(nodeRestfulErrors) {
+    const errors = [];
+    _.forIn(nodeRestfulErrors(), error => errors.push(errors.message));
+    return errors;
+}
 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
--- backend/config/routes.js
+++ backend/config/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 module.exports = (server) => {
     const router = express.Router();
-    
     //criando rotas
     server.use('/api', router);
     
     const billingCycleService = require('../api/billingCycle/billingCycleService')
     billingCycleService.register(router, '/billingCycles');
+
+    const billingSummaryService = require('../api/billingSummary/billingSummaryService');
+    router.route('/billingSummary').get(billingSummaryService.getSummary);
 }
